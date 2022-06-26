664b4fb9ec162297cc2ec2150baa8942669214f0
Victor <victorshiira4869@gmail.com>
2022-06-26 16:01:42 -0300

    Ciração de tela de cadastro de usuários

---
+++ LES Almoxarifado/Formularios/FormCadastroUsuario.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace LES_Almoxarifado.Formularios
+{
+    partial class FormCadastroUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSobrenome = new System.Windows.Forms.Label();
+            this.lblDatNasc = new System.Windows.Forms.Label();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.lblTipoUser = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.mskdTxtDatNasc = new System.Windows.Forms.MaskedTextBox();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.cbxTipoUser = new System.Windows.Forms.ComboBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(35, 27);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(45, 17);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblSobrenome
+            // 
+            this.lblSobrenome.AutoSize = true;
+            this.lblSobrenome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSobrenome.Location = new System.Drawing.Point(35, 93);
+            this.lblSobrenome.Name = "lblSobrenome";
+            this.lblSobrenome.Size = new System.Drawing.Size(81, 17);
+            this.lblSobrenome.TabIndex = 1;
+            this.lblSobrenome.Text = "Sobrenome";
+            // 
+            // lblDatNasc
+            // 
+            this.lblDatNasc.AutoSize = true;
+            this.lblDatNasc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatNasc.Location = new System.Drawing.Point(35, 161);
+            this.lblDatNasc.Name = "lblDatNasc";
+            this.lblDatNasc.Size = new System.Drawing.Size(134, 17);
+            this.lblDatNasc.TabIndex = 2;
+            this.lblDatNasc.Text = "Data de nascimento";
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCargo.Location = new System.Drawing.Point(35, 234);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(46, 17);
+            this.lblCargo.TabIndex = 3;
+            this.lblCargo.Text = "Cargo";
+            // 
+            // lblTipoUser
+            // 
+            this.lblTipoUser.AutoSize = true;
+            this.lblTipoUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoUser.Location = new System.Drawing.Point(353, 27);
+            this.lblTipoUser.Name = "lblTipoUser";
+            this.lblTipoUser.Size = new System.Drawing.Size(107, 17);
+            this.lblTipoUser.TabIndex = 4;
+            this.lblTipoUser.Text = "Tipo de usuário";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(353, 93);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(43, 17);
+            this.lblLogin.TabIndex = 5;
+            this.lblLogin.Text = "Login";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(353, 161);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(47, 17);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(38, 47);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(265, 20);
+            this.txtNome.TabIndex = 7;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(38, 113);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(265, 20);
+            this.txtSobrenome.TabIndex = 8;
+            // 
+            // mskdTxtDatNasc
+            // 
+            this.mskdTxtDatNasc.Location = new System.Drawing.Point(38, 182);
+            this.mskdTxtDatNasc.Mask = "##/##/####";
+            this.mskdTxtDatNasc.Name = "mskdTxtDatNasc";
+            this.mskdTxtDatNasc.Size = new System.Drawing.Size(100, 20);
+            this.mskdTxtDatNasc.TabIndex = 9;
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.Location = new System.Drawing.Point(38, 254);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(265, 20);
+            this.txtCargo.TabIndex = 10;
+            // 
+            // cbxTipoUser
+            // 
+            this.cbxTipoUser.FormattingEnabled = true;
+            this.cbxTipoUser.Items.AddRange(new object[] {
+            "Comum",
+            "Administrador"});
+            this.cbxTipoUser.Location = new System.Drawing.Point(356, 47);
+            this.cbxTipoUser.Name = "cbxTipoUser";
+            this.cbxTipoUser.Size = new System.Drawing.Size(166, 21);
+            this.cbxTipoUser.TabIndex = 11;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(356, 113);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(298, 20);
+            this.txtLogin.TabIndex = 12;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(356, 182);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(298, 20);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(249, 303);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(93, 30);
+            this.btnCadastrar.TabIndex = 14;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.IndianRed;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(367, 303);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(93, 30);
+            this.btnCancelar.TabIndex = 15;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // FormCadastroUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 361);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.cbxTipoUser);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(this.mskdTxtDatNasc);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblTipoUser);
+            this.Controls.Add(this.lblCargo);
+            this.Controls.Add(this.lblDatNasc);
+            this.Controls.Add(this.lblSobrenome);
+            this.Controls.Add(this.lblNome);
+            this.Name = "FormCadastroUsuario";
+            this.Text = "FormCadastroUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSobrenome;
+        private System.Windows.Forms.Label lblDatNasc;
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.Label lblTipoUser;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.MaskedTextBox mskdTxtDatNasc;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.ComboBox cbxTipoUser;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
