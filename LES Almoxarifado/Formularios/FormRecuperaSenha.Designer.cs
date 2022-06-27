a6d9ae755ecbf86ea05374ac8129764b433b9e01
Lucas Reis <69942329+Lukrys@users.noreply.github.com>
2022-06-26 23:02:37 -0300

    Recuperação de Senha

--- LES Almoxarifado/Formularios/FormRecuperaSenha.Designer.cs
+++ LES Almoxarifado/Formularios/FormRecuperaSenha.Designer.cs
@@ -29,12 +29,118 @@ namespace LES_Almoxarifado.Formularios
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CriarSenha = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Enviar código";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "E-mail de recuperação";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Código de verificação";
+            // 
+            // textBox2
+            // 
+            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox2.Location = new System.Drawing.Point(132, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(132, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Verificar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(323, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Um código de verificação será enviado para seu e-mail para\r\nautenticação e libera" +
+    "ção do acesso à criação de uma nova senha.";
+            // 
+            // CriarSenha
+            // 
+            this.CriarSenha.Location = new System.Drawing.Point(260, 202);
+            this.CriarSenha.Name = "CriarSenha";
+            this.CriarSenha.Size = new System.Drawing.Size(111, 23);
+            this.CriarSenha.TabIndex = 7;
+            this.CriarSenha.Text = "Criar nova senha";
+            this.CriarSenha.UseVisualStyleBackColor = true;
+            this.CriarSenha.Click += new System.EventHandler(this.CriarSenha_Click);
+            // 
+            // FormRecuperaSenha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(383, 260);
+            this.Controls.Add(this.CriarSenha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "FormRecuperaSenha";
             this.Text = "FormRecuperaSenha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button CriarSenha;
     }
 }
--- LES Almoxarifado/Formularios/FormRecuperaSenha.cs
+++ LES Almoxarifado/Formularios/FormRecuperaSenha.cs
@@ -12,9 +12,27 @@ namespace LES_Almoxarifado.Formularios
 {
     public partial class FormRecuperaSenha : Form
     {
+        static FormCriarSenha formCriarSenha = new FormCriarSenha();
+        static FormRecuperaSenha formRecuperaSenha = new FormRecuperaSenha();
         public FormRecuperaSenha()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CriarSenha_Click(object sender, EventArgs e)
+        {
+            formRecuperaSenha.Close();
+            formCriarSenha.Show();
         }
     }
 }
